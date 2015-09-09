3c18c030bab110b1c6850dc5873aef6d17fc421b
Addison Moyer <10541409@uvlink.uvu.edu>
2015-09-09 15:52:29 -0600

    Functions and Variables

---
+++ Iron Mantis DGM 1600/Assets/FunctionsAndVariables.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FunctionsAndVariables : MonoBehaviour 
+{
+	int myInt = 6;
+
+	void Start ()
+	{
+		myInt = MultiplyByThree (myInt);
+	 Debug.Log (myInt);
+	}
+
+	int MultiplyByThree (int number)
+	{
+		int ret;
+		ret = number * 3;
+		return ret;
+	}
+	
+}
---
+++ Iron Mantis DGM 1600/Assets/VarynFuncTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VarynFuncTest : MonoBehaviour {
+
+	int myInt = 49;
+	void Start ()
+	{
+		myInt = MultiplyByTwo (myInt);
+		Debug.Log (myInt);
+	}
+	int MultiplyByTwo (int number){
+	
+		int ret;
+		ret = number * 2;
+		return ret;
+	}
+
+
+}
