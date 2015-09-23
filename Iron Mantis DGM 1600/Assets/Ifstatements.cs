8dcdfd90738022d6347a3878338957c0c79f7282
Addison Moyer <addisonbmoyer@gmail.com>
2015-09-23 15:41:57 -0600

    If statements

---
+++ Iron Mantis DGM 1600/Assets/Ifstatements.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ifstatements : MonoBehaviour 
+{
+	float ChocholateTemperature = 85.0f;
+	float hotLimitTemperature = 70.0f;
+	float coldLimitTemperature = 40.0f;
+	
+	
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			TemperatureTest();
+		
+		ChocholateTemperature -= Time.deltaTime * 5f;
+	}
+	
+	
+	void TemperatureTest ()
+	{
+		// If the Chocholate's temperature is greater than the hottest drinking temperature...
+		if(ChocholateTemperature > hotLimitTemperature)
+		{
+			// ... do this.
+			print("Chocolate is too hot.");
+		}
+		// If it isn't, but the chocholate temperature is less than the coldest drinking temperature...
+		else if(ChocholateTemperature < coldLimitTemperature)
+		{
+			// ... do this.
+			print("Chocholate is too cold.");
+		}
+		// If it is neither of those then...
+		else
+		{
+			// ... do this.
+			print("Chocholate is just right.");
+		}
+	}
+}
---
+++ Iron Mantis DGM 1600/Assets/ifstatements1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ifstatements1 : MonoBehaviour 
+{
+	private float HowHigh = 3f;	
+	private float TooHigh = 5f;
+
+	void Start () 
+	{
+		Debug.Log (transform.position.y);
+		if (transform.position.y <= HowHigh)
+
+	{
+		print("Incomming!");
+	}
+		else if(transform.position.y >= TooHigh)
+	{
+		print("I'm flying!");
+	}
+
+		else 
+	{
+		print("I'm getting closer");
+	}
+	}
+}
