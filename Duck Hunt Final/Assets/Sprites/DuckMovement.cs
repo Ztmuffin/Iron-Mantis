d0bd85bc4337646be6593352d5545e05bca2548e
Addison Moyer <10541409@CS518-04.ad.uvu.edu>
2015-11-18 15:24:18 -0700

    ok

---
+++ Duck Hunt Final/Assets/Sprites/DuckMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckMovement : MonoBehaviour {
+	public float speed;
+	public Vector3 direction;
+
+	// Use this for initialization
+	void Start () 
+	{
+		DirectionChanger ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		transform.position = transform.position + (direction * speed);
+	}
+
+	public void DirectionChanger ()
+	{
+		direction = new Vector3 (Random.Range (-1,1), Random.Range(-.2f, 1f), 0);
+	}
+}
